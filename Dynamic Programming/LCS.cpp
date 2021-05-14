b79ccdfa160fd7a1fd2f9b3d1b9e9b58d836e3a7
Pawan Kumar Singh <pawan@pop-os.localdomain>
2021-05-15 00:13:56 +0530

    DP

--- Dynamic Programming/LCS.cpp
+++ Dynamic Programming/LCS.cpp
@@ -7,7 +7,7 @@ int LCS(string a, string b, int n, int m, int i, int j, vector<vector<int>> &dp)
 
 	if (dp[i][j] != -1)    return dp[i][j];
 
-	if (a[i] == b[j])    return dp[i][j] = 1 + LCS(a, b, n, m, i + 1, j, dp);
+	if (a[i] == b[j])    return dp[i][j] = 1 + LCS(a, b, n, m, i + 1, j + 1, dp);
 	else {
 		int op1 = LCS(a, b, n, m, i + 1, j, dp);
 		int op2 = LCS(a, b, n, m, i, j + 1, dp);
